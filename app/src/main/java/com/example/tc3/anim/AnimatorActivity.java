1db60398b3905159c0a3889cedf4ec1b3b961463
CC <tc3123>
2020-09-29 15:21:23 +0800

    属性动画

---
+++ app/src/main/java/com/example/tc3/anim/AnimatorActivity.java
@@ -0,0 +1,63 @@
+package com.tc3.tc3android;
+
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class AnimatorActivity extends AppCompatActivity {
+
+    @BindView(R.id.tvTest)
+    TextView tvTest;
+    @BindView(R.id.btnStart1)
+    Button btnStart1;
+    @BindView(R.id.btnStart2)
+    Button btnStart2;
+    @BindView(R.id.btnStart3)
+    Button btnStart3;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animator);
+        ButterKnife.bind(this);
+    }
+
+
+    @OnClick({R.id.btnStart2, R.id.btnStart3, R.id.btnStart1})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btnStart1:
+                ValueAnimator animation = ValueAnimator.ofFloat(0f, 100f);
+                animation.setDuration(1000);
+                animation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator updatedAnimation) {
+                        // You can use the animated value in a property that uses the
+                        // same type as the animation. In this case, you can use the
+                        // float value in the translationX property.
+                        float animatedValue = (float) updatedAnimation.getAnimatedValue();
+                        tvTest.setTranslationX(animatedValue);
+                    }
+                });
+                animation.start();
+                break;
+            case R.id.btnStart2:
+                ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(tvTest, "translationX", 100f);
+                objectAnimator.setDuration(1000);
+                objectAnimator.start();
+                break;
+            case R.id.btnStart3:
+                break;
+        }
+    }
+}
